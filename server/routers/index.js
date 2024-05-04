1ff80f9e9925d2ee3a2b7117683f4b80ed3e23fa
Amit <jangidamit058@gmail.com>
2024-05-04 23:16:41 +0530

    feat: added more apis

--- server/routers/index.js
+++ server/routers/index.js
@@ -4,7 +4,14 @@ const logger = require('winston').loggers.get('app')
 const registerUserService = require('../service/registerUserService')
 const createCarouselService = require('../service/createCarouselService')
 const getCarouselService = require('../service/getCarouselService')
-const scheduleHoursService = require('../service/scheduleHoursService')
+const createScheduleHoursService = require('../service/createScheduleHoursService')
+const getScheduleHoursService = require('../service/getScheduleHoursService')
+const getLocationCardsService = require('../service/getLocationCardsService')
+const getAboutUsService = require('../service/getAboutUsService')
+const getTestmonialsService = require('../service/gettestimonialsService')
+const getOurServiceService = require('../service/getOurServiceService')
+const getBlogsService = require('../service/getBlogsService')
+const getLocationService = require('../service/getLocationService')
 
 // Register user
 xcareRouter.post('/register',async (req, res) => {
@@ -22,8 +29,41 @@ xcareRouter.get('/carousel', async (req, res) => {
 })
 
 xcareRouter.post('/schedule_hours',async (req, res) => {
-    await scheduleHoursService(req, res)
+    await createScheduleHoursService(req, res)
 })
 
+// Get All Schedule Hours
+xcareRouter.get('/schedule_hours', async (req, res) => {
+    await getScheduleHoursService(req, res)
+})
 
+// Get All Location Cards
+xcareRouter.get('/location_cards', async (req, res) => {
+    await getLocationCardsService(req, res)
+})
+
+// Get About Us Page Data
+xcareRouter.get('/about_us', async (req, res) => {
+    await getAboutUsService(req, res)
+})
+
+// Get ALL TESTIMONIALS
+xcareRouter.get('/testimonials', async (req, res) => {
+    await getTestmonialsService(req, res)
+})
+
+// Get ALL OUR SERVICE
+xcareRouter.get('/our_services', async (req, res) => {
+    await getOurServiceService(req, res)
+})
+
+// Get ALL blogs
+xcareRouter.get('/blogs', async (req, res) => {
+    await getBlogsService(req, res)
+})
+
+// Get Locations
+xcareRouter.get('/locations', async (req, res) => {
+    await getLocationService(req, res)
+})
 module.exports = xcareRouter
---
+++ server/service/getAboutUsService.js
@@ -0,0 +1,12 @@
+const { pool } = require('../model/connection')
+
+module.exports = async (req, res) => {
+    const query = `SELECT * FROM about_us`
+    pool.query(query,(err, result) => {
+        if (err) {
+            res.status(500).json({ error: err })
+        } else {
+            return res.status(200).json(result)
+        }
+    })
+}
